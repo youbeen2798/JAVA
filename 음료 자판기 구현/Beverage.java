f2834b7b5cf1fcd84238f58ebaf0c6d24c5ad6bf
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-07-30 22:41:27 +0900

    Create Beverage.java

---
+++ 음료 자판기 구현/Beverage.java
@@ -0,0 +1,27 @@
+public abstract class Beverage extends Test {
+    
+    private int price;
+    private String name;
+
+    public Beverage(String name, int num, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public Integer getPrice(){
+        return this.price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void pay(PaymentMethod paymentMethod, int totalprice, int instanceNumber){ //음료 결제하는 메소드
+		    paymentMethod.pay(totalprice, instanceNumber);
+	  }
+
+}
