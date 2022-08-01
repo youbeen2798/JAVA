f7fb81d8b85504d277460590cda31eaa060a89a9
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-08-01 09:07:41 +0900

    Add files via upload

---
+++ 음료 자판기 구현/printSomething/PrintBeverageMenu.java
@@ -0,0 +1,18 @@
+package printSomething;
+public class PrintBeverageMenu {
+    
+    public static void PrintBeverageMenu(){
+
+        System.out.println("1. 아메리카노( 1200원 )");
+        System.out.println("2. 카페라떼( 1700원 )");
+        System.out.println("3. 모카치노( 1700원)");
+        System.out.println("4. 핫초코( 1500원 )");
+        System.out.println("5. 아이스아메리카노( 1700원 )");
+        System.out.println("6. 아이스초코( 1700원 )");
+        System.out.println("7. 아이스카페라떼( 1700원 )");
+        System.out.println("8. 복숭아아이스티( 1700원 )");
+
+
+    }
+
+}
