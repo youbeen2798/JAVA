839bb1fd740abd69ec9d04bccecece680c2c026a
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-08-01 09:07:32 +0900

    Create PaymentMenu.java

---
+++ 음료 자판기 구현/printSomething/PaymentMenu.java
@@ -0,0 +1,12 @@
+package printSomething;
+
+public class PaymentMenu {
+    
+    public static void PrintPaymentMenu(){
+
+        System.out.println("0. 현금");
+        System.out.println("1. 신용카드");
+        System.out.println("2. 온라인페이");
+       
+    }
+}
