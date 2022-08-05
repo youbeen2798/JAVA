13ec5af73fe03577526f4fa1b183ca7bb8d66cd0
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-08-05 19:15:52 +0900

    Create Divide.java

---
+++ 인터프리터 패턴 기반 사칙연산 구현/Divide.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+
+public class Divide implements Expression{
+    
+    //+의 왼쪽에 올 표현(숫자일 수도 있고 이미 연산된 결과일 수 있음)
+    Expression leftNumber;
+    
+    //+의 오른쪽에 올 표현
+    Expression rightNumber;
+
+    public Divide(Expression leftNumber, Expression rightNumber) {
+        this.leftNumber = leftNumber;
+        this.rightNumber = rightNumber;
+    }
+
+    @Override
+    public int interpret(Map variables) {
+        //왼쪽과 오른쪽의 결과를 더해서 반환
+        return leftNumber.interpret(variables) / rightNumber.interpret(variables);
+    }
+}
