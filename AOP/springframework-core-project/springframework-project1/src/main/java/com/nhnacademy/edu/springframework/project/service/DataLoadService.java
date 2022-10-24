c658fbec5280924718976c7dbb9a3a14831e4fb2
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-10-24 09:15:33 +0900

    Delete AOP directory

--- AOP/springframework-core-project/springframework-project1/src/main/java/com/nhnacademy/edu/springframework/project/service/DataLoadService.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.edu.springframework.project.service;
-
-import java.io.FileNotFoundException;
-
-public interface DataLoadService {
-    void loadAndMerge() throws FileNotFoundException;
-}
